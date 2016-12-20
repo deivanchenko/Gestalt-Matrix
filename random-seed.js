f9c2bf75666ff875c1e507a4624121040ee409ed
Denys <djonja@gmail.com>
2016-12-20 12:49:15 +0100

    Create random-seed.js

---
+++ random-seed.js
@@ -0,0 +1,23 @@
+var xoff = 0.0;
+var col = {
+
+  r: 155,
+  g: 40,
+  b: 103
+}
+
+function draw() {
+  createCanvas(40,40);
+  background(214);
+  col.r = random(100, 155);
+  col.g = random(0, 40);
+  col.b = random(0, 103);
+  xoff = xoff + .01;
+  var n = noise(xoff) * width;
+  var x = randomGaussian(50,50);
+  fill(col.r,col.g,col.b);
+  ellipse(x, n, n, n);
+  ellipse(n, x, n, n);
+  ellipse(n, n, x, n);
+  ellipse(n, n, n, x);
+}
